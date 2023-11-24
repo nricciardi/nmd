a850d8faeda3106fd4b240f5bacbf2425067226f
Nicola Ricciardi <ricciardincl@gmail.com>
2023-11-24 08:17:43 +0100

    Added parse implementation of ParsingRule

--- nmd-compiler/src/compiler/codex/parsing_result.rs
+++ nmd-compiler/src/compiler/codex/parsing_result.rs
@@ -2,13 +2,31 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ParsingError {
+    #[error("pattern provided '{0}' is invalid")]
+    InvalidPattern(&'static str),
+
     #[error("unknown error occurs")]
     Unknown
 }
 
 
 pub struct ParsingResultBody {
-
+    parsed_content: String
 }
 
+impl ParsingResultBody {
+    pub fn new(parsed_content: String) -> ParsingResultBody {
+        ParsingResultBody{
+            parsed_content
+        }
+    }
+
+    pub fn parsed_content(self: &Self) -> String {
+        self.parsed_content.clone()        // TODO: remove clone
+    }
+}
+
+
+
 pub type ParsingResult = Result<ParsingResultBody, ParsingError>;
+
--- nmd-compiler/src/compiler/codex/parsing_rule.rs
+++ nmd-compiler/src/compiler/codex/parsing_rule.rs
@@ -1,25 +1,57 @@
-use log::info;
+use log::{info, debug};
+use regex::Regex;
 
-pub use parsing_result::ParsingResult;
+pub use super::parsing_result::{ParsingResult, ParsingError, ParsingResultBody};
 
-use super::parsing_result;
 
 
 
 /// Rule to parse a NMD text based on a specific pattern matching rule
 pub struct ParsingRule {
-    matching_rule: String
+    search_pattern: &'static str,     // TODO: maybe &str
+    replacement_pattern: &'static str
 }
 
 impl ParsingRule {
-    pub fn new(matching_rule: String) -> Self {
+    pub fn new(search_pattern: &'static str, replacement_pattern: &'static str) -> Self {
+
+        debug!("created new parsing rule with search_pattern: '{}' and replacement_pattern: '{}'", search_pattern, replacement_pattern);
+
         ParsingRule {
-            matching_rule
+            search_pattern,
+            replacement_pattern
         }
     }
 
-    pub fn parse(content: &str) -> ParsingResult {
-        info!("will be parsed {}", content);
-        todo!()
+    pub fn parse(self: &Self, content: &str) -> ParsingResult {
+
+        let regex = match Regex::new(&self.search_pattern) {
+          Ok(r) => r,
+          Err(e) => return Err(ParsingError::InvalidPattern(self.search_pattern))  
+        };
+
+        let parsed_content = regex.replace_all(content, self.replacement_pattern).to_string();
+
+        debug!("parsed '{}' using '{}'->'{}'", content, self.search_pattern, self.replacement_pattern);
+        
+        Ok(ParsingResultBody::new(parsed_content))
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn parsing() {
+        let parsing_rule = ParsingRule::new(r"\*\*(.*?)\*\*", "<strong>$1</strong>");
+
+        let text_to_parse = r"A piece of **bold text**";
+
+        let parsed_text = parsing_rule.parse(text_to_parse).unwrap().parsed_content();
+
+        assert_eq!(parsed_text, r"A piece of <strong>bold text</strong>")
     }
 }
