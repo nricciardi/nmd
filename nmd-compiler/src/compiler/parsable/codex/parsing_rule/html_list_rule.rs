a12654e8525e7fc96fdd991923b38860a39fcf85
Nicola Ricciardi <ricciardincl@gmail.com>
2024-02-11 23:36:01 +0100

    Added List type

--- nmd-compiler/src/compiler/parsable/codex/parsing_rule/html_list_rule.rs
+++ nmd-compiler/src/compiler/parsable/codex/parsing_rule/html_list_rule.rs
@@ -1,3 +1,5 @@
+mod list;
+
 use std::sync::Arc;
 
 use crate::compiler::parsable::{codex::Modifier, ParsingConfiguration};
@@ -9,6 +11,14 @@ pub struct HtmlListRule {
 
 }
 
+impl HtmlListRule {
+    pub fn new() -> Self {
+        Self {
+            
+        }
+    }
+}
+
 impl ParsingRule for HtmlListRule {
     fn modifier(&self) -> &Modifier {
         todo!()
@@ -16,5 +26,31 @@ impl ParsingRule for HtmlListRule {
 
     fn parse(&self, content: &str, parsing_configuration: Arc<ParsingConfiguration>) -> Result<ParsingOutcome, ParsingError> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::list::List;
+
+    #[test]
+    fn parsing() {
+        let nmd_text = r#"
+
+- element 1
+- element 2
+    - element 2.1
+        - element 2.1.1a
+        element 2.1.1b
+        - element 2.1.2
+        - element 2.1.3
+    - element 2.2
+"#.trim();
+
+        let root_list = List::default();
+
+        
+
+
     }
 }
