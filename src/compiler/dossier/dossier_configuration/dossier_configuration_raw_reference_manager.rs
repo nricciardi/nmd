f04b992262dd48b69d21e4a67d4da526354751c3
nricciardi <ricciardincl@gmail.com>
2024-05-02 12:07:21 +0200

    Feature step

--- src/compiler/dossier/dossier_configuration/dossier_configuration_raw_reference_manager.rs
+++ src/compiler/dossier/dossier_configuration/dossier_configuration_raw_reference_manager.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{PathBuf, MAIN_SEPARATOR_STR};
 
 use super::dossier_configuration_raw_reference::DossierConfigurationRawReference;
 
@@ -14,6 +14,17 @@ impl DossierConfigurationRawReferenceManager {
     }
 
     pub fn set_root_path(&mut self, root_path: PathBuf) {
+
+        self.not_unix_like_os = false;
+
+        if MAIN_SEPARATOR_STR.eq(r"/") {     // => unix-like
+            log::info!("UNIX-like OS inferred: no logical separator replacement");
+
+        } else {     // => windows
+            log::info!("Windows OS inferred: it will be applied a logical separator replacement (from {} to /)", MAIN_SEPARATOR_STR);
+            self.not_unix_like_os = true;
+        }
+
         self.root_path = Some(root_path)
     }
 
